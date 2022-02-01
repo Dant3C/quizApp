92938169f2feaff93a4ca576c25de7df77aab597
dant3C <dcerlez1231@gmail.com>
2022-02-01 09:41:17 -0800

    added an animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-
-	for(let n = 1; n <= 5; n++)
+		let p = 1;
+	for(let n = 1; n <= 4; n++)
 	{
 	
 	$("#div"+n).click(function(){	
@@ -8,12 +8,26 @@ $(document).ready(function(){
 	});
 	}
 	$("#btn1").click(function(){
+
 		if($("#test1").val() == 85){
-			alert("correct");
-		}else{
+			$("#ani").css("visibility","visible");
+			var div = $("#ani");
+			div.animate({left: '300px'}, "slow");
+			div.animate({height:'400px'},"slow");
+			div.animate({width:'1200'},"slow");
+			div.animate({fontSize:'8em'},"slow");
+			// alert("correct");
+		}else if(p == 3){
 		alert("wrong, its 85");
+		}else{
+			p = p +1;
+			alert("wrong")
 		}
+		
 
 	});
+	$("#flip").click(function(){
+    $("#panel").slideDown("slow");
+  });
 	
 });
